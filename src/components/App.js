0c62224804799798f6da1084f6137b62acced5c2
Aisha Imtiyaz <144771688+aishaimtiyaz@users.noreply.github.com>
2024-08-21 14:13:01 +0530

    Update App.js

--- src/components/App.js
+++ src/components/App.js
@@ -9,30 +9,32 @@ const [lname,setLname] = useState("");
 const [relationship,setRelationship] = useState("");
 const [flag,setFlag] = useState(true);
 
-function calculateRealtionship(e)
-{
+function calculateRealtionship(e) {
   e.preventDefault();
 
-      let str1 = fname;
-      let str2 = lname;
-  
-   for(let char of str1)
-    {
-      if(char.includes(str2))
-        {
-          str1=str1.replace(char,"");
-          str2=str2.replace(char,"");
-        }
+  let str1 = fname.split('');
+  let str2 = lname.split('');
+
+  // Remove common characters
+  str1 = str1.filter(char => {
+    const indexInStr2 = str2.indexOf(char);
+    if (indexInStr2 !== -1) {
+      str2.splice(indexInStr2, 1); // Remove character from str2
+      return false; // Remove from str1
     }
-    if((str1.length)>0 && str2.length>0)
-      {
-        setRelationship(arr[((str1+str2).length)%6]);
-        setFlag(true);
-      }
-    else
+    return true; // Keep in str1
+  });
+
+  const remainingLength = str1.length + str2.length;
+  
+  if (remainingLength === 0) {
     setFlag(false);
-      
+  } else {
+    setRelationship(arr[remainingLength % 6]);
+    setFlag(true);
+  }
 }
+
 
 function resetAll()
 {
